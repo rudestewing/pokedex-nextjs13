c8da4f16ec8040277319143f8a1a7ff14f4ae72b
Rudi Setiawan <33628446+rudestewing@users.noreply.github.com>
2023-07-07 16:43:45 +0700

    filter

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,21 @@
 import { apiRequest } from '@/utils/apiRequest';
 
-export const fetchPokemonList = async (params: object) => {
-  const response = await apiRequest({ path: '/pokemon', method: 'GET', params });
-  return response.data.results;
+export const fetchPokemonList = async (params: any) => {
+  if (params.type) {
+    const response = await apiRequest({ path: `/type/${params.type}`, method: 'GET' });
+    return (response.data.pokemon || []).map((item: any) => item.pokemon);
+  } else {
+    const response = await apiRequest({ path: '/pokemon', method: 'GET', params });
+    return response.data.results;
+  }
 };
 
 export const getPokemonByNameApi = async (name: string) => {
   const response = await apiRequest({ path: `/pokemon/${name}`, method: 'GET' });
   return response.data;
+};
+
+export const getTypesApi = async () => {
+  const response = await apiRequest({ path: '/type', method: 'GET' });
+  return response.data.results;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,123 @@
 'use client';
 
-import { fetchPokemonList } from '@/api';
+import { fetchPokemonList, getTypesApi } from '@/api';
 import PokemonListItem from '@/components/PokemonListItem';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import Image from 'next/image';
-import { useCallback, useState } from 'react';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { Button, Drawer, Form, Radio } from 'antd';
+import { useCallback, useMemo, useState } from 'react';
+
+const limit = 20;
 
 export default function Home() {
-  const filter = {
-    limit: 20,
-  };
+  const [formFilter] = Form.useForm();
 
-  const { hasNextPage, fetchNextPage, data, isLoading } = useInfiniteQuery({
-    queryKey: ['pokemon-list'],
-    queryFn: (context) => {
-      console.log('ctx', context);
-      return fetchPokemonList({
-        offset: context.pageParam || 0,
-        limit: filter.limit,
-      });
-    },
-    getNextPageParam: (lastPage, allPages) => {
-      if ((lastPage || []).length === filter.limit) {
-        const params = (allPages || []).flat().length;
-        console.log('params', params);
-        return params;
-      } else {
-        return undefined;
-      }
-    },
-    getPreviousPageParam: (firstPage, allPages) => {},
+  const [showedFilter, setShowedFilter] = useState(false);
+  const [filter, setFilter] = useState({
+    type: '',
   });
 
+  const hasFilter = useMemo(() => {
+    return !!filter.type;
+  }, [filter]);
+
+  const { data: types } = useQuery(['types'], () => getTypesApi());
+
+  const { hasNextPage, fetchNextPage, data, isLoading, refetch, remove, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['pokemon-list', filter],
+      queryFn: (context) => {
+        return fetchPokemonList({
+          ...filter,
+          offset: context.pageParam || 0,
+        });
+      },
+      getNextPageParam: (lastPage, allPages) => {
+        if ((lastPage || []).length >= limit && !filter.type) {
+          const params = (allPages || []).flat().length;
+          return params;
+        } else {
+          return undefined;
+        }
+      },
+      getPreviousPageParam: (firstPage, allPages) => {},
+    });
+
   const handleLoadMore = useCallback(() => {
-    fetchNextPage();
-  }, [fetchNextPage]);
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage]);
+
+  const handleShowedFilter = useCallback(
+    (isVisible: boolean) => () => setShowedFilter(isVisible),
+    []
+  );
+
+  const handleApplyFilter = useCallback((values: any) => {
+    setFilter((state) => ({ ...state, type: values.type }));
+    setShowedFilter(false);
+  }, []);
 
   return (
-    <div className="py-4">
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-3 max-w-screen-md mx-auto">
-        {(data?.pages || []).map((page: any) => {
-          return page.map((item: any) => {
-            return <PokemonListItem pokemon={item} key={item.name} />;
-          });
-        })}
+    <>
+      <div className="py-4 max-w-screen-md mx-auto flex flex-col gap-2">
+        <div className="flex justify-end">
+          <Button onClick={handleShowedFilter(true)} type={hasFilter ? 'primary' : 'default'}>
+            Filter
+          </Button>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 ">
+          {(data?.pages || []).map((page: any) => {
+            return page.map((item: any) => {
+              return <PokemonListItem pokemon={item} key={item.name} />;
+            });
+          })}
+        </div>
+        {hasNextPage && (
+          <div className="flex justify-center p-4">
+            <Button loading={isLoading || isFetchingNextPage} onClick={handleLoadMore}>
+              Load More
+            </Button>
+          </div>
+        )}
       </div>
-      <div className="flex justify-center p-4">
-        <button
-          type="button"
-          onClick={handleLoadMore}
-          className="cursor-pointer px-3 py-1 bg-blue-500 text-white rounded-md"
-          disabled={isLoading}
-        >
-          load more
-        </button>
-      </div>
-    </div>
+      <Drawer
+        placement="right"
+        open={showedFilter}
+        footer={
+          <div className="flex gap-1 justify-end">
+            <Button onClick={() => formFilter.submit()}>
+              <span>Apply</span>
+            </Button>
+          </div>
+        }
+        onClose={handleShowedFilter(false)}
+      >
+        <div>
+          <Form
+            form={formFilter}
+            onFinish={handleApplyFilter}
+            wrapperCol={{ span: 24 }}
+            labelCol={{ span: 24 }}
+          >
+            <Form.Item label="Pokemon Type" name="type">
+              <Radio.Group
+                optionType="button"
+                options={[
+                  { key: 0, value: '', label: 'All' },
+                  ...(types || []).map((item: any) => {
+                    return {
+                      key: item.name,
+                      value: item.name,
+                      label: item.name,
+                    };
+                  }),
+                ]}
+              />
+            </Form.Item>
+          </Form>
+        </div>
+      </Drawer>
+    </>
   );
 }
